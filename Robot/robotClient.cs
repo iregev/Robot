58f32cb685950a082594c8c67b113a9981d4e49b
Idan Regev <iregev@microsoft.com>
2021-10-12 12:17:44 +0300

    parse form

--- Robot/robotClient.cs
+++ Robot/robotClient.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Net.Http;
+using HtmlAgilityPack;
 
 namespace Robot
 {
@@ -14,10 +15,14 @@ namespace Robot
 
         public async Task<string> MoveStr(int pivotDegrees, double meters)
         {
-            var responseString = await client.GetStringAsync("http://google.com");
+            var responseString = await client.GetStringAsync("http://ynet.co.il");
+            HtmlDocument doc = new HtmlDocument();
+            doc.LoadHtml(responseString);
+            var txt = doc.DocumentNode.SelectSingleNode("//span[@class='author']");
+
             //var metersMoved = await new HttpClient().Send();
             //return metersMoved == meters;
-            return responseString;
+            return txt.InnerText;
 
         }
     }
